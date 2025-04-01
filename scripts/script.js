1dac73cac04b517043dced93873b40266e8f3d08
Nour-Alhajali <nour.dev.work@gmail.com>
2025-04-01 07:20:34 +0300

    feat: Add initial calculator structure and styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,48 @@
-let nothing;
+function add(a, b) {
+  a = +a;
+  b = +b;
+
+  return a + b;
+}
+
+function substract(a, b) {
+  a = +a;
+  b = +b;
+
+  return a - b;
+}
+
+function multiply(a, b) {
+  a = +a;
+  b = +b;
+
+  return a * b;
+}
+
+function divide(a, b) {
+  a = +a;
+  b = +b;
+
+  return Math.round((a / b) * 1000) / 1000;
+}
+
+function operate(a, b, operator) {
+  let result;
+  switch (operater) {
+    case "+":
+      result = add(a, b);
+      break;
+    case "-":
+      result = substract(a, b);
+      break;
+    case "*":
+      result = multiply(a, b);
+      break;
+    case "/":
+      result = divide(a, b);
+      break;
+    default:
+      result = "Invalid Operator";
+  }
+  return result;
+}
